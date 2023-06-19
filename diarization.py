00a3410eb82755a77598948aee3859a83a9819c6
Brono25 <brono_25@hotmail.com>
2023-06-19 14:15:48 +1000

    added cuda check

--- diarization.py
+++ diarization.py
@@ -1,23 +1,22 @@
-
 import sys
 from pathlib import Path
 from pyannote.audio import Pipeline
 import torch
+
 # Make sure to visit these URLs and accept the user conditions and create an access token
 # hf.co/pyannote/speaker-diarization
 # hf.co/pyannote/segmentation
 # hf.co/settings/tokens
 
-
-print('MPS Available:',torch.backends.mps.is_available())
-print('MPS Built:',torch.backends.mps.is_built())
-
+# Check if CUDA is available and set device to GPU if it is
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+print('Running on device:', device)
 
 # Instantiate pretrained speaker diarization pipeline
-
 pipeline = Pipeline.from_pretrained("pyannote/speaker-diarization@2.1",
                                     use_auth_token="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")
-#pipeline.to("mps")  # Use "mps" for Apple M1 GPU
+
+pipeline = pipeline.to(device)  # Move the pipeline model to GPU if available
 
 # Get file_path from command line
 if len(sys.argv) > 1:
