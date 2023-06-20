3f1ca8abe2504c876e7f065aee1ff0cfc143550e
Brono <b.ashford@student.unsw.edu.au>
2023-06-20 14:37:47 +1000

    tested on katana

--- test.sh
+++ test.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-DATA_PATH='/srv/scratch/z5146619/'
+DATA_PATH='/srv/scratch/z5146619'
 
 for wav_file in "${DATA_PATH}/eng_wav/"* "${DATA_PATH}/spa_wav/"*; do
 
     filename=$(basename $wav_file)
-    echo $wav_file
+    echo $filename
 
 done
 
