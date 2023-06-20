ae9ffa49fd9e885c4866aea7cb3558481b3eeb6d
Brono25 <brono_25@hotmail.com>
2023-06-20 14:35:17 +1000

    editing task

--- test.sh
+++ test.sh
@@ -1,37 +1,11 @@
 #!/bin/bash
 
-#PBS -N mp3_2_wav
-#PBS -l select=1:ncpus=4:ngpus=1:mem=16gb
-#PBS -l walltime=01:00:00
-#PBS -M b.ashford@student.unsw.edu.au
-#PBS -m ae
-#PBS -j oe
-#PBS -o /home/z5146619/katana/output_rttm
+DATA_PATH='/srv/scratch/z5146619/'
 
-# load necessary modules
-module load ffmpeg || exit 1
+for wav_file in "${DATA_PATH}/eng_wav/"* "${DATA_PATH}/spa_wav/"*; do
 
-cd $PBS_O_WORKDIR
+    filename=$(basename $wav_file)
+    echo $wav_file
 
-DEST_PATH_ENG="/home/z5146619/data/eng_wav"
-DEST_PATH_SPA="/home/z5146619/data/spa_wav"
-
-for file in /home/z5146619/data/eng_mp3/* ; do
-    ext="${file##*.}"
-    ext="${ext,,}"
-    if [[ $ext == 'mp3' ]]; then
-        filename=$(basename -- "$file" .mp3)
-        echo "Processing $file"
-        echo ffmpeg -i "$file" "$DEST_PATH_ENG/${filename}.wav" || echo "Failed to convert $file"
-    fi
 done
 
-for file in /home/z5146619/data/spa_mp3/* ; do
-    ext="${file##*.}"
-    ext="${ext,,}"
-    if [[ $ext == 'mp3' ]]; then
-        filename=$(basename -- "$file" .mp3)
-        echo "Processing $file"
-        echo ffmpeg -i "$file" "$DEST_PATH_SPA/${filename}.wav" || echo "Failed to convert $file"
-    fi
-done
