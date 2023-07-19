68eb652864d2e51a0e5eee2dc29d951ebeece693
Brono25 <brono_25@hotmail.com>
2023-07-19 22:25:01 +1000

    added root dir so it can run from any location

--- tools/metrics/test_bench.sh
+++ tools/metrics/test_bench.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
 
+root="/Users/brono/GitHub/katana/tools/metrics"
+cd "$root"
 source "/Users/brono/miniconda3/etc/profile.d/conda.sh"
 conda activate thesis-env
 
 for pyfile in test_files/*.py; do
     python $pyfile || exit 1
- 
 done
 
 echo "-------ALL PASS-------"
