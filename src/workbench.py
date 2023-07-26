0a6512d4faa443a04fd34c327bd6e476a2b590c0
Brono25 <brono_25@hotmail.com>
2023-07-26 19:44:54 +1000

    created areas to work on

--- src/workbench.py
+++ src/workbench.py
@@ -1,5 +1,6 @@
 from pyannote.core import Annotation, Segment
 import os
+from pyannote.database.util import load_rttm
 
 # Always use CS-SpeakerDiarization-Thesis as root
 import sys
@@ -11,15 +12,16 @@ sys.path.append(root)
 # local imports
 from src.transcript import (  # noqa: E402
     Transcript,
-    cha_to_transcript,
-    save_transcript_to_file,
+    convert_cha_to_transcript,
 )  # noqa: E402
-from src.utilities import ( # noqa: E402
+from src.utilities import (  # noqa: E402
     ROOT_DIR,
     CHA_FILES_DIR,
+    HYP_RTTM_DIR,
     debug_transcript_comparison,
-)  
-
+)
+from src.cs_metrics import CSMetrics
+from src.language_error import LanguageError
 
 # Process files one at a time
 uri = "sastre09"
@@ -28,7 +30,28 @@ if not os.path.isfile(cha_file):
     print(f"{cha_file} not found.")
 
 
-# Create Transcript and RTTM files
-transcript = cha_to_transcript(cha_file=cha_file)
-#debug_transcript_comparison(transcript)
-# save_transcript_to_file(transcript=transcript)
+# --------------Create Transcript and RTTM files-------------- #
+transcript = convert_cha_to_transcript(cha_file=cha_file)
+transcript.save_transcript_to_file()
+transcript.export_ref_rttm(support=True)
+transcript.export_lang_rttm(support=True)
+ref = transcript.get_ref_annotation(support=True)
+lang = transcript.get_language_annotation(support=True)
+hyp = load_rttm(f"{HYP_RTTM_DIR}/hyp_{uri}_1.rttm")[uri + '_1']
+
+
+# --------------Code-Switching Metrics-------------- #
+metrics = CSMetrics(transcript=transcript)
+print(f"I-Index: {metrics.i_index() * 100:.2f}%")
+print(f"M-Index: {metrics.m_index()* 100:.2f}%")
+print(f"Burstiness: {metrics.burstiness()* 100:.2f}%")
+
+
+# --------------Language Errors-------------- #
+analysis = LanguageError(
+    uri=uri, reference=ref, hypothesis=hyp, language_annotation=lang
+)
+components = analysis.compute_components()
+results = analysis.compute_metric(components=components)
+for k, v in results.items():
+    print(f"{k} = {v * 100:.2f} %")
