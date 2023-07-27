ac933841913935f8c4e458ecab0c8ba5e02d8707
Brono25 <brono_25@hotmail.com>
2023-07-27 22:52:50 +1000

    set up test area

--- src/testbench.py
+++ src/testbench.py
@@ -9,5 +9,4 @@ root = re.search(r"(.*/CS-SpeakerDiarization-Thesis)", __file__).group(1)
 sys.path.append(root)
 
 from src.transcript import Transcript
-from src.utilities import get_uri_of_file, get_primary_language_of_file, LOG_FILES
 
