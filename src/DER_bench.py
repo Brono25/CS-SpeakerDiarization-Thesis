8ca9004c76c1fad4cb244947767df35d92053f1c
Brono25 <brono_25@hotmail.com>
2023-08-24 12:11:26 +1000

    used info

--- src/DER_bench.py
+++ src/DER_bench.py
@@ -1,94 +1,107 @@
 # Always use CS-SpeakerDiarization-Thesis as root
 import sys
 import re
-from pyannote.core import Annotation
 from pyannote.database.util import load_rttm
 from pyannote.metrics.diarization import DiarizationErrorRate
 import matplotlib.pyplot as plt
+
 root = re.search(r"(.*/CS-SpeakerDiarization-Thesis)", __file__).group(1)
 sys.path.append(root)
 from functions.utilities import plot_annotations
-# local imports
-from functions.transcript import (  # noqa: E402
-    Transcript,
-    convert_cha_to_transcript,
-    reduce_transcript,
-)
+
 from functions.cs_diarization_metrics import (
     CSDiarizationMetrics,
     timeline_to_annotation,
 )
 
 # --------------------SETUP--------------------
+ROOT = "/Users/brono/GitHub/cs-dataset/code-switched/sastre01"
 uri = "sastre01"
-DIR = "/Users/brono/GitHub/cs-dataset/code-switched/sastre01"
-reference_rttm_file = f"{DIR}/ref_{uri}.rttm"
-ref = load_rttm(reference_rttm_file)[uri]
+ref_rttm_path = f"{ROOT}/ref_sastre01.rttm"
+lang_rttm_path = f"{ROOT}/lang_sastre01.rttm"
+hyp_rttm_path = f"{ROOT}/pyannote/sastre01_pyannote.rttm"
 
-hypothesis_rttm_file = f"{DIR}/pyannote/{uri}_pyannote.rttm"
-hyp = load_rttm(hypothesis_rttm_file)[uri]
 
-language_rttm_file = f"{DIR}/ref_lang_{uri}.rttm"
-lang = load_rttm(language_rttm_file)[uri]
+# ---------------------------------------------
+
+info = {
+    "uri": uri,
+    "root": ROOT,
+    "ref_rttm": load_rttm(ref_rttm_path)[uri],
+    "lang_rttm": load_rttm(lang_rttm_path)[uri],
+    "hyp_rttm": load_rttm(hyp_rttm_path)[uri],
+}
 
 
 # --------------------FUNCTIONS--------------------
-def perform_language_error_rates(ref, hyp, lang, uri):
+def perform_language_error_rates(info):  # ref and hyp need matching labels
+    uri = info["uri"]
+    ref = info["ref_rttm"]
+    hyp = info["hyp_rttm"]
+    lang = info["lang_rttm"]
+
     cs_metrics = CSDiarizationMetrics(
         reference=ref, hypothesis=hyp, language_annotation=lang, uri=uri
     )
     components = cs_metrics.compute_components()
     metric = cs_metrics.compute_metric(components)
     for k, v in metric.items():
-        print(f"{k} = {v * 100:.2f}")
+        print(f"{k} = {v * 100:.3f}")
 
 
-def perform_confusion_analysis(uri, ref, hyp, lang, output):
+def perform_confusion_analysis(info):  # ref and hyp need matching labels
+    uri = info["uri"]
+    ref = info["ref_rttm"]
+    hyp = info["hyp_rttm"]
+    lang = info["lang_rttm"]
+
     cs_metrics = CSDiarizationMetrics(
         reference=ref, hypothesis=hyp, language_annotation=lang, uri=uri
     )
     confusion_tl = cs_metrics._get_confusion_timeline()
     confusion_ann = timeline_to_annotation(uri, confusion_tl, "CONF")
-    with open(output, "w") as file:
+    with open(f"{info['root']}/conf_{info['uri']}.rttm", "w") as file:
         confusion_ann.write_rttm(file)
 
 
-def perform_missed_analysis(uri, ref, hyp, lang, output):
+def perform_missed_analysis(info):  # ref and hyp need matching labels
+    uri = info["uri"]
+    ref = info["ref_rttm"]
+    hyp = info["hyp_rttm"]
+    lang = info["lang_rttm"]
     cs_metrics = CSDiarizationMetrics(
         reference=ref, hypothesis=hyp, language_annotation=lang, uri=uri
     )
     miss_tl = cs_metrics._get_confusion_timeline()
     miss_ann = timeline_to_annotation(uri, miss_tl, "MISS")
-    with open(output, "w") as file:
+    with open(f"{info['root']}/miss_{info['uri']}.rttm", "w") as file:
         miss_ann.write_rttm(file)
 
 
 def plot_ref_vs_hyp(ref, hyp):
-    
-    plot_annotations([(ref, "Reference"),(hyp, "Hypothesis")])
+    plot_annotations([(ref, "Reference"), (hyp, "Hypothesis")])
 
-def print_detailed_der(reference, hypothesis):
-    # Initialize DiarizationErrorRate metric
-    metric = DiarizationErrorRate(skip_overlap=True, collar=0.5)
-    components = metric.compute_components(reference, hypothesis)
 
-    print("Missed Detection:", components['missed detection'])
-    print("False Alarm:", components['false alarm'])
-    print("Confusion:", components['confusion'])
-    der = metric(reference, hypothesis)
-    print("Overall DER:", der)
+def detailed_der(info):
+    ref = info["ref_rttm"]
+    hyp = info["hyp_rttm"]
+    output = f'{info["root"]}/der_{info["uri"]}.txt'
+    metric = DiarizationErrorRate(uriskip_overlap=True, collar=0.5)
+    components = metric.compute_components(ref, hyp)
+
+    with open(output, "w") as file:
+        file.write(f"Missed Detection: {components['missed detection']}\n")
+        file.write(f"False Alarm: {components['false alarm']}\n")
+        file.write(f"Confusion: {components['confusion']}\n")
+        der = metric(ref, hyp)
+        file.write(f"Overall DER: {der}\n")
+
+    print(f"DER details have been saved to {output}")
 
 
 
 # --------------------ANALYSIS--------------------
 
 
-
-perform_language_error_rates(ref=ref, hyp=hyp, lang=lang, uri=uri)
-
-output = f"{DIR}/pyannote/{uri}_miss_pyannote.rttm"
-perform_missed_analysis(uri=uri, ref=ref, hyp=hyp, lang=lang, output=output)
-output = f"{DIR}/pyannote/{uri}_conf_pyannote.rttm"
-perform_confusion_analysis(uri=uri, ref=ref, hyp=hyp,lang=lang,  output=output)
-
-print_detailed_der(reference=ref, hypothesis=hyp)
+if __name__ == "__main__":
+    detailed_der(info)
