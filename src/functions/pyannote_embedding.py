4d0912d308faff17c94bf673ea53e5e58219cb88
Brono25 <brono_25@hotmail.com>
2023-10-04 12:18:24 +1100

    plotting embeddings

--- src/functions/pyannote_embedding.py
+++ src/functions/pyannote_embedding.py
@@ -1,11 +1,79 @@
-from pyannote.audio import Model
-from pyannote.audio import Inference
-from pyannote.audio.pipelines.utils.getter import get_model
+from pyannote.audio import Model, Inference
+from pyannote.database.util import load_rttm
+from sklearn.manifold import TSNE
+import matplotlib.pyplot as plt
+import numpy as np
+import re
+from collections import defaultdict
+import pickle
+
+# Always use CS-SpeakerDiarization-Thesis as root
+import sys
+import re
+
+root = re.search(r"(.*/CS-SpeakerDiarization-Thesis)", __file__).group(1)
+sys.path.append(root)
+
+SCIKIT_ORANGE = "#FE8025"
+SCIKIT_DARK_ORANGE = "#7F4013"
+SCIKIT_BLUE = "#387EB5"
+SCIKIT_DARK_BLUE = "#1C3F5A"
+
+YELLOW_ORANGE = "#FCB73B"
+
+INTENSE_ORANGE = "#EF582C"
+TEAL = "#25B7B2"
+
+SPEAKER1_SPA = "#0172F1"
+SPEAKER1_ENG = "#36BBF4"
+
+SPEAKER2_SPA = "#E6A538"
+SPEAKER2_ENG = "#EB8E29"
+
 
 
 ACCESS_TOKEN = "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"
-MODEL = "speechbrain/spkrec-ecapa-voxceleb"
+uri_list = ['sastre01', 'herring06', 'herring07', 'herring08', 'herring10', 'herring13', 'sastre11', 'zeledon04', 'sastre06', 'sastre09', 'zeledon08', 'zeledon14']
 
+model = Model.from_pretrained("pyannote/embedding", use_auth_token=ACCESS_TOKEN)
+inference = Inference(model, window="whole")
 
-model = get_model(MODEL, use_auth_token=ACCESS_TOKEN)
+for uri in uri_list:
+    embeddings = []  
+    label_map = []
+    audio = f"/Users/brono/GitHub/cs-dataset/code-switched/{uri}/{uri}.wav"
+    lang_rttm = load_rttm(f"/Users/brono/GitHub/cs-dataset/code-switched/{uri}/lang_{uri}.rttm")[uri]
+    ref = load_rttm(f"/Users/brono/GitHub/cs-dataset/code-switched/{uri}/ref_{uri}.rttm")[uri]
 
+    overlaps = ref.get_overlap()
+    ref = ref.extrude(overlaps)
+    lang_rttm = lang_rttm.extrude(overlaps)
+    speaker1, speaker2 = [x for x in ref.labels() if isinstance(x, str) and re.match(r"[A-Z]{3}", x)]
+    lang1, lang2 = [x for x in lang_rttm.labels() if isinstance(x, str) and re.match(r"SPA|ENG", x)]
+
+    ref_list = [(segment, label) for segment, _, label in ref.itertracks(yield_label=True)]
+    lang_list = [(segment, lang) for segment, _, lang in lang_rttm.itertracks(yield_label=True)]
+
+    assert len(ref_list) == len(lang_list)
+
+    min_length = 1.0  
+    for i, (segment, label) in enumerate(ref_list):
+        lang_seg, lang = lang_list[i]
+        assert lang_seg == segment
+        if segment.end - segment.start >= min_length:
+            if lang is not np.nan:
+                label_map.append((label, lang))
+                embedding = inference.crop(audio, segment)
+                embeddings.append(embedding)
+            
+    embeddings_array = np.vstack(embeddings)
+
+    bundle = {
+        'embeddings': embeddings_array,
+        'label_map': label_map,
+        'speaker1': speaker1,
+        'speaker2': speaker2
+    }
+
+    with open(f"embeddings/{uri}_embedding.pkl", 'wb') as f:
+       pickle.dump(bundle, f)
---
+++ src/functions/pyannote_embedding_plot.py
@@ -0,0 +1,128 @@
+from sklearn.manifold import TSNE
+import matplotlib.pyplot as plt
+import re
+from collections import defaultdict
+import sys
+import re
+import pickle
+import glob
+
+root = re.search(r"(.*/CS-SpeakerDiarization-Thesis)", __file__).group(1)
+sys.path.append(root)
+
+ORANGE = "#FF8100"
+LIGHT_ORANGE = "#FBCA04"
+LIGHT_BLUE = "#79DCFF"
+BLUE = "#007EFF"
+
+SPEAKER1_SPA = LIGHT_BLUE
+SPEAKER1_ENG = BLUE
+SPEAKER2_SPA = ORANGE
+SPEAKER2_ENG = LIGHT_ORANGE
+
+def load_embedding(filename):
+    with open(filename, "rb") as f:
+        bundle = pickle.load(f)
+    return bundle
+
+def get_colours(data):
+    colour_map = defaultdict(dict)
+    colour_map[data["speaker1"]]["SPA"] = SPEAKER1_SPA
+    colour_map[data["speaker1"]]["ENG"] = SPEAKER1_ENG
+    colour_map[data["speaker2"]]["SPA"] = SPEAKER2_SPA
+    colour_map[data["speaker2"]]["ENG"] = SPEAKER2_ENG
+    colours = []
+    for label, lang in data["label_map"]:
+        colours.append(colour_map[label][lang])
+    return colours
+
+from sklearn.manifold import TSNE
+import matplotlib.pyplot as plt
+import re
+from collections import defaultdict
+import sys
+import re
+import pickle
+import glob
+
+root = re.search(r"(.*/CS-SpeakerDiarization-Thesis)", __file__).group(1)
+sys.path.append(root)
+
+file_list = [
+"embeddings/herring08_embedding.pkl", 
+"embeddings/sastre01_embedding.pkl",
+"embeddings/herring13_embedding.pkl",
+"embeddings/zeledon14_embedding.pkl",
+"embeddings/sastre06_embedding.pkl",
+"embeddings/sastre09_embedding.pkl",
+"embeddings/sastre11_embedding.pkl",
+"embeddings/zeledon08_embedding.pkl",
+"embeddings/herring06_embedding.pkl",
+"embeddings/zeledon04_embedding.pkl",
+"embeddings/herring10_embedding.pkl",
+"embeddings/herring07_embedding.pkl",
+]
+
+
+
+
+ORANGE = "#FF8100"
+LIGHT_ORANGE = "#FBCA04"
+LIGHT_BLUE = "#79DCFF"
+BLUE = "#007EFF"
+
+SPEAKER1_SPA = LIGHT_BLUE
+SPEAKER1_ENG = BLUE
+SPEAKER2_SPA = ORANGE
+SPEAKER2_ENG = LIGHT_ORANGE
+
+def load_embedding(filename):
+    with open(filename, "rb") as f:
+        bundle = pickle.load(f)
+    return bundle
+
+def get_colours(data):
+    colour_map = defaultdict(dict)
+    colour_map[data["speaker1"]]["SPA"] = SPEAKER1_SPA
+    colour_map[data["speaker1"]]["ENG"] = SPEAKER1_ENG
+    colour_map[data["speaker2"]]["SPA"] = SPEAKER2_SPA
+    colour_map[data["speaker2"]]["ENG"] = SPEAKER2_ENG
+    colours = []
+    for label, lang in data["label_map"]:
+        colours.append(colour_map[label][lang])
+    return colours
+
+
+fig, axes = plt.subplots(3, 4, figsize=(15, 10)) 
+
+for idx, file in enumerate(file_list):
+    data = load_embedding(file)
+    colours = get_colours(data)
+
+    ax = axes[idx // 4, idx % 4]
+
+    tsne = TSNE(n_components=2, random_state=1)
+    X_tsne = tsne.fit_transform(data["embeddings"])
+
+    for i, (x, y) in enumerate(X_tsne):
+        ax.scatter(x, y, c=colours[i])
+
+    ax.set_title(file.split('/')[-1].replace('_embedding.pkl', ''), fontsize=12)
+    ax.tick_params(axis='both', which='major', labelsize=8)
+
+legend_labels = {
+    SPEAKER1_SPA: f"\nSpeaker 1\n Spanish\n",
+    SPEAKER1_ENG: f"\nSpeaker 1\n English\n",
+    SPEAKER2_SPA: f"\nSpeaker 2\n Spanish\n",
+    SPEAKER2_ENG: f"\nSpeaker 2\n English\n"
+}
+handles = [plt.Line2D([0], [0], marker='o', color='w', label=legend_labels[color], markersize=11, markerfacecolor=color) for color in legend_labels]
+fig.legend(handles=handles, loc='lower center', fontsize=12, labelspacing=0.5, ncol=len(legend_labels), frameon=False)  # Set ncol and turned off frame
+
+fig.suptitle("Code-Switching Speech Embeddings", fontsize=30, y=0.99)  # Increase font size and adjust y value
+
+fig.subplots_adjust(top=0.9)  # Adjust the top margin to push subplots down
+
+plt.show()
+
+
